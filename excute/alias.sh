cff4f5f9d8bb2a1af55856d1362f95f199f0c14c
Karel Muniz <75614258+karelmuniz@users.noreply.github.com>
2021-02-10 11:52:43 +0100

    add docker loging

--- excute/alias.sh
+++ excute/alias.sh
@@ -3,3 +3,4 @@
 alias httpie='http --verify=no'
 alias gitpullall='find . -type d -depth 1 -exec echo "{}" \; -exec git -C {} pull \;'
 alias rollback='git reset --hard HEAD~1'
+dockerlogin='docker login -u ocpadmin -p $(oc whoami -t) registry.dev.westeurope.bright-shopper.nl'
