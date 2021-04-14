2093b4c7018379f74cdd12f052072642b03d9e27
Karel Muniz <karel.muniz@icemobile.com>
2021-04-14 20:14:09 +0200

    New Alias

--- execute/alias.sh
+++ execute/alias.sh
@@ -4,3 +4,13 @@ alias httpie='http --verify=no'
 alias gitpullall='find . -type d -depth 1 -exec echo "{}" \; -exec git -C {} pull \;'
 alias rollback='git reset --hard HEAD~1'
 alias dockerlogin='docker login -u ocpadmin -p $(oc whoami -t) registry.dev.westeurope.bright-shopper.nl'
+
+alias.p=push
+alias.st=status -sb
+alias.ll=log --oneline
+alias.cm=commit -m
+alias.rv=remote -v
+alias.d=diff
+alias.gl=config --global -l
+alias.se=!git rev-list --all | xargs git grep -F
+alias.rb=reset --hard HEAD~1
